8f2accde7c7365b970db3f42ef4695ce769aab0f
rnadal <rullyeah@gmail.com>
2017-04-22 15:22:27 +0200

    Eureka! I've a point!

--- src/Domain/Point.php
+++ src/Domain/Point.php
@@ -4,7 +4,14 @@ namespace AStar\Domain;
 
 class Point
 {
-    
+    protected $x;
+    protected $y;
+
+    public function __construct(int $x, int $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
 }
 
 ?>
--- tests/PointTest.php
+++ tests/PointTest.php
@@ -6,10 +6,34 @@ use PHPUnit\Framework\TestCase;
 
 class PointTest extends TestCase
 {
+    protected $point;
+
+    public function setUp()
+    {
+        $this->point = new Point(0,0);
+    }
+
     public function testNew()
     {
-        $bean = new Point();
+        $bean = new Point(0,0);
         $this->assertInstanceOf(Point::class, $bean);
+    }
+
+    /**
+     * @dataProvider invalidCoordsProvider
+     */
+    public function testInvalidCoordsLaunchException($x, $y)
+    {
+        $this->expectException(\TypeError::class);
+        $bean = new Point($x,$y);
+    }
+
+    public function invalidCoordsProvider()
+    {
+        return [
+            'Nada'  => [null, null],
+            'string' => ['asd', 'asd']
+        ];
     }
 }
 ?>
